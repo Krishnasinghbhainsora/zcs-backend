32a9f7469aaaec2fb4e2f3f347202cd8ea2993d5
Krishnasinghbhainsora <krishnabhainsora@gmail.com>
2025-01-28 21:44:44 +0530

    first commit

--- middlewares/Upload.js
+++ middlewares/Upload.js
@@ -1,26 +1,26 @@
 const multer = require("multer");
+const path = require("path");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, "uploads/"); // Ensure this path exists
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
+    cb(null, `${Date.now()}-${file.originalname}`);
   },
 });
 
-const fileFilter = (req, file, cb) => {
-  const fileTypes = /pdf|doc|docx/;
-  const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = fileTypes.test(file.mimetype);
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    const fileTypes = /pdf|doc|docx/;
+    const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = fileTypes.test(file.mimetype);
 
-  if (extname && mimetype) {
-    cb(null, true);
-  } else {
-    cb("Only .pdf, .doc, .docx formats are allowed!");
-  }
-};
-
-const upload = multer({ storage, fileFilter }).single("resume");
-
-module.exports = upload;
+    if (extname && mimetype) {
+      return cb(null, true);
+    } else {
+      cb("Only .pdf, .doc, .docx formats are allowed!");
+    }
+  },
+});
